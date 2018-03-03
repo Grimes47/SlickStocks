2e96d3dac2691b9c2bf4f0711b5cd9a083fa47f5
Adam <arhogan@outlook.com>
2018-03-03 14:30:15 -0800

    Add pull-to-refresh
    Added pull-to-refresh functionality on main view controller (pull to update listed stock prices) with delay so user has a chance to see the text displayed on the refresher.

--- SlickStocks/MainViewController/MainViewController.swift
+++ SlickStocks/MainViewController/MainViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var tableView: UITableView!
     
+    lazy var refresher: UIRefreshControl = {
+        let refresher = UIRefreshControl()
+        refresher.addTarget(self, action: #selector(refreshStockPrices(_:)), for: .valueChanged)
+        refresher.attributedTitle = NSAttributedString(string: "Refreshing Stock Prices...")
+        
+        return refresher
+    }()
     
+    @objc private func refreshStockPrices(_ sender: Any) {
+        downloadStockPerformanceData(forTickers: defaultTickerSymbols)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = refresher
+        } else {
+            tableView.addSubview(refresher)
+        }
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         downloadStockPerformanceData(forTickers: defaultTickerSymbols)
@@ -20,15 +41,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     let defaultTickerSymbols = ["NKE","MSFT","AAPL","GOOG","ORCL","COST","AMZN","WMT","GE","XOM","CVX","GM","VLO","BA","WAG","AIG","PFE","DOW","UPS","DELL","LMT","BBY","DIS"]
     var performanceData = [MainStockPerformanceData]()
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
     func downloadStockPerformanceData(forTickers ticker: [String]) {
         
@@ -43,12 +55,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             guard let data = data else { return }
             do {
                 let jsonData = try JSONDecoder().decode(StockQuotes.self, from: data)
+                // Append parsed JSON data into global array for retrieval
                 for index in jsonData.stockQuotes {
                     self.performanceData.append(index)
                 }
                 print(self.performanceData)
-                DispatchQueue.main.async {
+                // Delay reload/end refreshing for 3/4 second until so user has chance to see refresher text
+                let delayRefresh = DispatchTime.now() + .milliseconds(750)
+                DispatchQueue.main.asyncAfter(deadline: delayRefresh) {
                     self.tableView.reloadData()
+                    self.refresher.endRefreshing()
                 }
                 
             } catch let error {
