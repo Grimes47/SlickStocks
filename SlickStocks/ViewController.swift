dfd207fd58ea2f10167e293f4930734341f5170d
Adam <arhogan@outlook.com>
2018-03-02 16:13:30 -0800

    Initial Commit

---
+++ SlickStocks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlickStocks
+//
+//  Created by Adam Hogan on 3/2/18.
+//  Copyright © 2018 Adam Hogan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
