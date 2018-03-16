6069e0626f4cea9a033467a938f0c32dc0340800
Adam <arhogan@outlook.com>
2018-03-15 17:10:18 -0700

    Initial commit of additional test
    Tests for the async processes and also to test the models.

--- SlickStocksTests/SlickStocksTests.swift
+++ SlickStocksTests/SlickStocksTests.swift
@@ -13,10 +13,16 @@ class SlickStocksTests: XCTestCase {
     
     let mainVC = MainViewController()
     var stockQuotes: StockQuotes?
+    var detailStockData: DetailStockPerformanceData?
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
     
     func testDecimalConversion() {
@@ -52,9 +58,36 @@ class SlickStocksTests: XCTestCase {
         XCTAssertEqual(string, converted)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testBatchStockDownloads() {
+        let tickers = mainVC.defaultTickerSymbols
+        let expectation = XCTestExpectation(description: "Download batch stock data")
+        NetworkCalls.shared.downloadBatchStockPerformanceData(forTickers: tickers) { (completion) in
+            self.stockQuotes = completion
+            XCTAssertNotNil(self.stockQuotes, "Data not downloaded properly")
+            expectation.fulfill()
+        }
+        let res = XCTWaiter.wait(for: [expectation], timeout: 5.0)
+        if res == .completed {
+            XCTAssertNotNil(stockQuotes, "Failed to download batch stock data")
+        } else {
+            XCTAssert(false, "The call to get the URL ran into some other error")
+        }
+    }
+    
+    func testSingleStockDownloads() {
+        let ticker = "NKE"
+        let expectation = XCTestExpectation(description: "Download single stock data")
+        NetworkCalls.shared.downloadSingleStockPerformanceData(forTicker: ticker) { (completion) in
+            self.detailStockData = completion
+            XCTAssertNotNil(self.detailStockData, "Data not downloaded properly")
+            expectation.fulfill()
+        }
+        let res = XCTWaiter.wait(for: [expectation], timeout: 5.0)
+        if res == .completed {
+            XCTAssertNotNil(detailStockData, "Failed to download single stock data")
+        } else {
+            XCTAssert(false, "The call to get the URL ran into some other error")
+        }
     }
     
 }
